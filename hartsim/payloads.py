fa6046f79b69a628811b190b22d68c265730a983
gr0ker <gr0ker@yandex.ru>
2023-04-08 12:33:46 +0500

    Add 1 to 4 size support for unsigned payload

--- hartsim/payloads.py
+++ hartsim/payloads.py
@@ -1,7 +1,13 @@
 from abc import abstractmethod
+from functools import reduce
+from itertools import repeat
 from typing import Iterator
 
 FULL_BYTE_MASK = 0xFF
+U16_MASK = 0xFFFF
+MIN_INTEGER_SIZE = 1
+MAX_INTEGER_SIZE = 4
+BITS_IN_BYTE = 8
 
 
 class Payload:
@@ -18,10 +24,17 @@ class Payload:
         pass
 
 
-class U8(Payload):
+class Unsigned(Payload):
     def __init__(self,
-                 value: int = 0):
+                 value: int = 0,
+                 size: int = MIN_INTEGER_SIZE):
+        self.__size = MIN_INTEGER_SIZE if size < MIN_INTEGER_SIZE else MAX_INTEGER_SIZE if size > MAX_INTEGER_SIZE else size
+        self.__mask = reduce(lambda x, y: x << 8 | y,
+                             repeat(FULL_BYTE_MASK, self.__size))
         self.set_value(value)
+
+    def get_size(self):
+        return self.__size
 
     def get_value(self):
         return self.__value
@@ -29,14 +42,19 @@ class U8(Payload):
     def set_value(self, value):
         if value < 0:
             value = -value
-        self.__value = value & FULL_BYTE_MASK
+        self.__value = value & self.__mask
 
     def __next__(self):
-        if self._offset == 0:
+        if self._offset < self.__size:
+            next = (self.__value >> (BITS_IN_BYTE *
+                    (self.__size - self._offset - 1))) & FULL_BYTE_MASK
             self._offset += 1
-            return self.__value & FULL_BYTE_MASK
+            return next
         else:
             raise StopIteration
 
     def deserialize(self, iterator: Iterator[int]):
-        self.set_value(next(iterator) & FULL_BYTE_MASK)
+        value = 0
+        for _ in range(0, self.__size):
+            value = (value << BITS_IN_BYTE) | (next(iterator) & FULL_BYTE_MASK)
+        self.set_value(value)
--- tests/test_payloads.py
+++ tests/test_payloads.py
@@ -1,57 +1,231 @@
 import unittest
 
-from hartsim import U8
+from hartsim import Unsigned
 
 
 class TestPayloads(unittest.TestCase):
 
-    def test_u8_default_value_is_zero(self):
+    def test_unsigned_default_value_is_zero(self):
         expected = 0
-        target = U8()
+        target = Unsigned()
         self.assertEqual(target.get_value(), expected)
 
-    def test_u8_non_default_value(self):
+    def test_unsigned_default_size_is_one(self):
+        expected = 1
+        target = Unsigned()
+        self.assertEqual(target.get_size(), expected)
+
+    def test_unsigned_min_size_is_one(self):
+        size = 0
+        expected = 1
+        target = Unsigned(size=size)
+        self.assertEqual(target.get_size(), expected)
+
+    def test_unsigned_max_size_is_four(self):
+        size = 5
+        expected = 4
+        target = Unsigned(size=size)
+        self.assertEqual(target.get_size(), expected)
+
+    def test_unsigned_non_default_value_1_byte(self):
         expected = value = 123
-        target = U8(value)
+        target = Unsigned(value)
         self.assertEqual(target.get_value(), expected)
 
-    def test_u8_negative_value(self):
+    def test_unsigned_non_default_value_2_bytes(self):
+        size = 2
+        expected = value = 256
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_non_default_value_3_bytes(self):
+        size = 3
+        expected = value = 65536
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_non_default_value_4_bytes(self):
+        size = 4
+        expected = value = 16777216
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_max_value_4_bytes(self):
+        size = 4
+        expected = value = 4294967295
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_negative_value_1_byte(self):
         value = -123
         expected = 123
-        target = U8(value)
+        target = Unsigned(value)
         self.assertEqual(target.get_value(), expected)
 
-    def test_u8_wider_value(self):
+    def test_unsigned_negative_value_2_bytes(self):
+        size = 2
+        value = -256
+        expected = 256
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_negative_value_3_bytes(self):
+        size = 3
+        value = -65536
+        expected = 65536
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_negative_value_4_bytes(self):
+        size = 4
+        value = -16777216
+        expected = 16777216
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_max_negative_value_4_bytes(self):
+        size = 4
+        value = -4294967295
+        expected = 4294967295
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_wider_value_1_byte(self):
         value = 257
         expected = 1
-        target = U8(value)
+        target = Unsigned(value)
         self.assertEqual(target.get_value(), expected)
 
-    def test_u8_wider_negative_value(self):
+    def test_unsigned_wider_value_2_bytes(self):
+        size = 2
+        value = 65537
+        expected = 1
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_wider_value_3_bytes(self):
+        size = 3
+        value = 16777217
+        expected = 1
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_wider_value_4_bytes(self):
+        size = 4
+        value = 4294967297
+        expected = 1
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_wider_negative_value_1_byte(self):
         value = -257
         expected = 1
-        target = U8(value)
+        target = Unsigned(value)
         self.assertEqual(target.get_value(), expected)
 
-    def test_u8_deserialize(self):
-        serialized = bytearray([1, 2, 3, 4, 5])
-        expected = 4
-        target = U8()
+    def test_unsigned_wider_negative_value_2_bytes(self):
+        size = 2
+        value = -65537
+        expected = 1
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_wider_negative_value_3_bytes(self):
+        size = 3
+        value = -16777217
+        expected = 1
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_wider_negative_value_4_bytes(self):
+        size = 4
+        value = -4294967297
+        expected = 1
+        target = Unsigned(value, size)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_deserialize_1_byte(self):
+        serialized = bytearray([0x01, 0x02, 0x03, 0x04, 0x05])
+        expected = 0x02
+        target = Unsigned()
         serialized_iterator = iter(serialized)
-        next(serialized_iterator)
-        next(serialized_iterator)
         next(serialized_iterator)
         target.deserialize(serialized_iterator)
         self.assertEqual(target.get_value(), expected)
 
-    def test_u8_serialize(self):
-        value = 231
-        expected = bytearray([value])
+    def test_unsigned_deserialize_2_bytes(self):
+        size = 2
+        serialized = bytearray([0x01, 0x02, 0x03, 0x04, 0x05])
+        expected = 0x0203
+        target = Unsigned(size=size)
+        serialized_iterator = iter(serialized)
+        next(serialized_iterator)
+        target.deserialize(serialized_iterator)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_deserialize_3_bytes(self):
+        size = 3
+        serialized = bytearray([0x01, 0x02, 0x03, 0x04, 0x05])
+        expected = 0x020304
+        target = Unsigned(size=size)
+        serialized_iterator = iter(serialized)
+        next(serialized_iterator)
+        target.deserialize(serialized_iterator)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_deserialize_4_bytes(self):
+        size = 4
+        serialized = bytearray([0x01, 0x02, 0x03, 0x04, 0x05])
+        expected = 0x02030405
+        target = Unsigned(size=size)
+        serialized_iterator = iter(serialized)
+        next(serialized_iterator)
+        target.deserialize(serialized_iterator)
+        self.assertEqual(target.get_value(), expected)
+
+    def test_unsigned_serialize_1_byte(self):
+        size = 1
+        value = 0x05
+        expected = bytearray([0x05])
         expectedIndex = 0
-        target = U8(value)
+        target = Unsigned(value)
         for item in target:
-            self.assertEqual(item, expected[expectedIndex])
+            self.assertEqual(item, expected[expectedIndex], f"{expectedIndex}")
             expectedIndex += 1
+        self.assertEqual(expectedIndex, size)
+
+    def test_unsigned_serialize_2_bytes(self):
+        size = 2
+        value = 0x0504
+        expected = bytearray([0x05, 0x04])
+        expectedIndex = 0
+        target = Unsigned(value, size)
+        for item in target:
+            self.assertEqual(item, expected[expectedIndex], f"{expectedIndex}")
+            expectedIndex += 1
+        self.assertEqual(expectedIndex, size)
+
+    def test_unsigned_serialize_3_bytes(self):
+        size = 3
+        value = 0x050403
+        expected = bytearray([0x05, 0x04, 0x03])
+        expectedIndex = 0
+        target = Unsigned(value, size)
+        for item in target:
+            self.assertEqual(item, expected[expectedIndex], f"{expectedIndex}")
+            expectedIndex += 1
+        self.assertEqual(expectedIndex, size)
+
+    def test_unsigned_serialize_4_bytes(self):
+        size = 4
+        value = 0x05040302
+        expected = bytearray([0x05, 0x04, 0x03, 0x02])
+        expectedIndex = 0
+        target = Unsigned(value, size)
+        for item in target:
+            self.assertEqual(item, expected[expectedIndex], f"{expectedIndex}")
+            expectedIndex += 1
+        self.assertEqual(expectedIndex, size)
 
 
 if __name__ == '__main__':
