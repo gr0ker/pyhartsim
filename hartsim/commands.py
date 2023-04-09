0f72761068e016fc3ef61fa4738046c0f918add0
gr0ker <gr0ker@yandex.ru>
2023-04-09 20:40:17 +0500

    Add command 9

--- hartsim/commands.py
+++ hartsim/commands.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from .payloads import F32, U16, U24, U8, Ascii, PackedAscii, PayloadSequence
+from .payloads import F32, U16, U24, U32, U8, Ascii, PackedAscii, PayloadSequence
 
 polling_address = U8(0)
 long_address = bytearray([0x26, 0x06, 0x12, 0x34, 0x56])
@@ -50,6 +50,110 @@ def handle_request(command_number: int, data: bytearray) -> bytearray:
         payload = Cmd7Reply()
     elif command_number == 8:
         payload = Cmd8Reply()
+    elif command_number == 9:
+        request = Cmd9Request()
+        request.deserialize(iter(data))
+        payload = Cmd9Reply()
+        payload.device_variable_code_1.set_value(
+            request.device_variable_code_1.get_value())
+        if request.device_variable_code_2.is_skipped():
+            payload.device_variable_code_2.skip()
+            payload.device_variable_classification_2.skip()
+            payload.device_variable_units_2.skip()
+            payload.device_variable_value_2.skip()
+            payload.device_variable_status_2.skip()
+        else:
+            payload.device_variable_code_2.set_value(
+                request.device_variable_code_2.get_value())
+            payload.device_variable_code_2.include()
+            payload.device_variable_classification_2.include()
+            payload.device_variable_units_2.include()
+            payload.device_variable_value_2.include()
+            payload.device_variable_status_2.include()
+        if request.device_variable_code_3.is_skipped():
+            payload.device_variable_code_3.skip()
+            payload.device_variable_classification_3.skip()
+            payload.device_variable_units_3.skip()
+            payload.device_variable_value_3.skip()
+            payload.device_variable_status_3.skip()
+        else:
+            payload.device_variable_code_3.set_value(
+                request.device_variable_code_3.get_value())
+            payload.device_variable_code_3.include()
+            payload.device_variable_classification_3.include()
+            payload.device_variable_units_3.include()
+            payload.device_variable_value_3.include()
+            payload.device_variable_status_3.include()
+        if request.device_variable_code_4.is_skipped():
+            payload.device_variable_code_4.skip()
+            payload.device_variable_classification_4.skip()
+            payload.device_variable_units_4.skip()
+            payload.device_variable_value_4.skip()
+            payload.device_variable_status_4.skip()
+        else:
+            payload.device_variable_code_4.set_value(
+                request.device_variable_code_4.get_value())
+            payload.device_variable_code_4.include()
+            payload.device_variable_classification_4.include()
+            payload.device_variable_units_4.include()
+            payload.device_variable_value_4.include()
+            payload.device_variable_status_4.include()
+        if request.device_variable_code_5.is_skipped():
+            payload.device_variable_code_5.skip()
+            payload.device_variable_classification_5.skip()
+            payload.device_variable_units_5.skip()
+            payload.device_variable_value_5.skip()
+            payload.device_variable_status_5.skip()
+        else:
+            payload.device_variable_code_5.set_value(
+                request.device_variable_code_5.get_value())
+            payload.device_variable_code_5.include()
+            payload.device_variable_classification_5.include()
+            payload.device_variable_units_5.include()
+            payload.device_variable_value_5.include()
+            payload.device_variable_status_5.include()
+        if request.device_variable_code_6.is_skipped():
+            payload.device_variable_code_6.skip()
+            payload.device_variable_classification_6.skip()
+            payload.device_variable_units_6.skip()
+            payload.device_variable_value_6.skip()
+            payload.device_variable_status_6.skip()
+        else:
+            payload.device_variable_code_6.set_value(
+                request.device_variable_code_6.get_value())
+            payload.device_variable_code_6.include()
+            payload.device_variable_classification_6.include()
+            payload.device_variable_units_6.include()
+            payload.device_variable_value_6.include()
+            payload.device_variable_status_6.include()
+        if request.device_variable_code_7.is_skipped():
+            payload.device_variable_code_7.skip()
+            payload.device_variable_classification_7.skip()
+            payload.device_variable_units_7.skip()
+            payload.device_variable_value_7.skip()
+            payload.device_variable_status_7.skip()
+        else:
+            payload.device_variable_code_7.set_value(
+                request.device_variable_code_7.get_value())
+            payload.device_variable_code_7.include()
+            payload.device_variable_classification_7.include()
+            payload.device_variable_units_7.include()
+            payload.device_variable_value_7.include()
+            payload.device_variable_status_7.include()
+        if request.device_variable_code_8.is_skipped():
+            payload.device_variable_code_8.skip()
+            payload.device_variable_classification_8.skip()
+            payload.device_variable_units_8.skip()
+            payload.device_variable_value_8.skip()
+            payload.device_variable_status_8.skip()
+        else:
+            payload.device_variable_code_8.set_value(
+                request.device_variable_code_8.get_value())
+            payload.device_variable_code_8.include()
+            payload.device_variable_classification_8.include()
+            payload.device_variable_units_8.include()
+            payload.device_variable_value_8.include()
+            payload.device_variable_status_8.include()
     elif command_number == 12:
         payload = Cmd12Reply()
     elif command_number == 20:
@@ -129,6 +233,66 @@ class Cmd8Reply (PayloadSequence):
     sv_classification: F32 = sv_classification
     tv_classification: U8 = tv_classification
     qv_classification: F32 = qv_classification
+
+
+@dataclass
+class Cmd9Request (PayloadSequence):
+    device_variable_code_1: U8 = U8()
+    device_variable_code_2: U8 = U8(is_optional=True)
+    device_variable_code_3: U8 = U8(is_optional=True)
+    device_variable_code_4: U8 = U8(is_optional=True)
+    device_variable_code_5: U8 = U8(is_optional=True)
+    device_variable_code_6: U8 = U8(is_optional=True)
+    device_variable_code_7: U8 = U8(is_optional=True)
+    device_variable_code_8: U8 = U8(is_optional=True)
+
+
+@dataclass
+class Cmd9Reply (PayloadSequence):
+    response_code: U8 = U8()
+    device_status: U8 = device_status
+    extended_device_status: U8 = extended_device_status
+    device_variable_code_1: U8 = U8()
+    device_variable_classification_1: U8 = U8()
+    device_variable_units_1: U8 = U8()
+    device_variable_value_1: F32 = F32()
+    device_variable_status_1: U8 = U8()
+    device_variable_code_2: U8 = U8(is_optional=True)
+    device_variable_classification_2: U8 = U8(is_optional=True)
+    device_variable_units_2: U8 = U8(is_optional=True)
+    device_variable_value_2: F32 = F32(is_optional=True)
+    device_variable_status_2: U8 = U8(is_optional=True)
+    device_variable_code_3: U8 = U8(is_optional=True)
+    device_variable_classification_3: U8 = U8(is_optional=True)
+    device_variable_units_3: U8 = U8(is_optional=True)
+    device_variable_value_3: F32 = F32(is_optional=True)
+    device_variable_status_3: U8 = U8(is_optional=True)
+    device_variable_code_4: U8 = U8(is_optional=True)
+    device_variable_classification_4: U8 = U8(is_optional=True)
+    device_variable_units_4: U8 = U8(is_optional=True)
+    device_variable_value_4: F32 = F32(is_optional=True)
+    device_variable_status_4: U8 = U8(is_optional=True)
+    device_variable_code_5: U8 = U8(is_optional=True)
+    device_variable_classification_5: U8 = U8(is_optional=True)
+    device_variable_units_5: U8 = U8(is_optional=True)
+    device_variable_value_5: F32 = F32(is_optional=True)
+    device_variable_status_5: U8 = U8(is_optional=True)
+    device_variable_code_6: U8 = U8(is_optional=True)
+    device_variable_classification_6: U8 = U8(is_optional=True)
+    device_variable_units_6: U8 = U8(is_optional=True)
+    device_variable_value_6: F32 = F32(is_optional=True)
+    device_variable_status_6: U8 = U8(is_optional=True)
+    device_variable_code_7: U8 = U8(is_optional=True)
+    device_variable_classification_7: U8 = U8(is_optional=True)
+    device_variable_units_7: U8 = U8(is_optional=True)
+    device_variable_value_7: F32 = F32(is_optional=True)
+    device_variable_status_7: U8 = U8(is_optional=True)
+    device_variable_code_8: U8 = U8(is_optional=True)
+    device_variable_classification_8: U8 = U8(is_optional=True)
+    device_variable_units_8: U8 = U8(is_optional=True)
+    device_variable_value_8: F32 = F32(is_optional=True)
+    device_variable_status_8: U8 = U8(is_optional=True)
+    timestamp: U32 = U32()
 
 
 @dataclass
