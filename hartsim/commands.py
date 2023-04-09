39e5d084b8e9ea46789496b5ce78b2213d87198e
gr0ker <gr0ker@yandex.ru>
2023-04-09 17:39:31 +0500

    Add read commands 2-12 (skip 9, 11)

--- hartsim/commands.py
+++ hartsim/commands.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from .payloads import F32, U16, U24, U8, Ascii, PayloadSequence
+from .payloads import F32, U16, U24, U8, Ascii, PackedAscii, PayloadSequence
 
-short_address = 0
+polling_address = U8(0)
 long_address = bytearray([0x26, 0x06, 0x12, 0x34, 0x56])
 is_burst_mode = False
 
@@ -11,8 +11,28 @@ device_status = U8()
 extended_device_status = U8()
 config_change_counter = U16(0)
 
+loop_current_mode = U8(1)
+
 pv_units = U8(12)
 pv_value = F32(1.2345)
+pv_classification = U8(65)
+
+sv_units = U8(32)
+sv_value = F32(23.4567)
+sv_classification = U8()
+
+tv_units = U8(240)
+tv_value = F32(345)
+tv_classification = U8()
+
+qv_units = U8(12)
+qv_value = F32(1.2345)
+qv_classification = U8()
+
+loop_current = F32(4.321)
+percent_of_range = F32(0.0200625)
+
+message = PackedAscii(32, "THIS IS A MESSAGE")
 
 long_tag = Ascii(32, "This is a HART device simulator")
 
@@ -48,10 +68,58 @@ class Cmd1Reply (PayloadSequence):
 
 
 @dataclass
+class Cmd2Reply (PayloadSequence):
+    response_code: U8 = U8()
+    device_status: U8 = device_status
+    loop_current: U8 = loop_current
+    percent_of_range: F32 = percent_of_range
+
+
+@dataclass
+class Cmd3Reply (PayloadSequence):
+    response_code: U8 = U8()
+    device_status: U8 = device_status
+    loop_current: U8 = loop_current
+    pv_units: U8 = pv_units
+    pv_value: F32 = pv_value
+    sv_units: U8 = sv_units
+    sv_value: F32 = sv_value
+    tv_units: U8 = tv_units
+    tv_value: F32 = tv_value
+    qv_units: U8 = qv_units
+    qv_value: F32 = qv_value
+
+
+@dataclass
+class Cmd7Reply (PayloadSequence):
+    response_code: U8 = U8()
+    device_status: U8 = device_status
+    polling_address: U8 = polling_address
+    loop_current_mode: U8 = loop_current_mode
+
+
+@dataclass
+class Cmd8Reply (PayloadSequence):
+    response_code: U8 = U8()
+    device_status: U8 = device_status
+    pv_classification: U8 = pv_classification
+    sv_classification: F32 = sv_classification
+    tv_classification: U8 = tv_classification
+    qv_classification: F32 = qv_classification
+
+
+@dataclass
+class Cmd12Reply (PayloadSequence):
+    response_code: U8 = U8()
+    device_status: U8 = device_status
+    message: PackedAscii = message
+
+
+@dataclass
 class Cmd20Reply (PayloadSequence):
     response_code: U8 = U8()
     device_status: U8 = device_status
-    long_tag: U8 = long_tag
+    long_tag: Ascii = long_tag
 
 
 @dataclass
--- hartsim/hartsim.py
+++ hartsim/hartsim.py
@@ -4,8 +4,9 @@ import time
 from .config import Configuration
 from .framingutils import FrameType, HartFrame
 from .payloads import U8
-from .commands import Cmd0Reply, Cmd1Reply, Cmd20Reply, ErrorReply, long_address,\
-    short_address, is_burst_mode
+from .commands import Cmd0Reply, Cmd12Reply, Cmd1Reply, Cmd20Reply, Cmd2Reply,\
+    Cmd3Reply, Cmd7Reply, Cmd8Reply, ErrorReply, long_address, polling_address,\
+    is_burst_mode
 
 config = Configuration()
 
@@ -27,12 +28,22 @@ while True:
         long_address_matched = request.is_long_address\
             and request.long_address == long_address
         short_address_matched = not request.is_long_address\
-            and request.short_address == short_address
+            and request.short_address == polling_address.get_value()
         if long_address_matched or short_address_matched:
             if request.command_number == 0:
                 payload = Cmd0Reply()
             elif request.command_number == 1:
                 payload = Cmd1Reply()
+            elif request.command_number == 2:
+                payload = Cmd2Reply()
+            elif request.command_number == 3:
+                payload = Cmd3Reply()
+            elif request.command_number == 7:
+                payload = Cmd7Reply()
+            elif request.command_number == 8:
+                payload = Cmd8Reply()
+            elif request.command_number == 12:
+                payload = Cmd12Reply()
             elif request.command_number == 20:
                 payload = Cmd20Reply()
             else:
@@ -40,7 +51,7 @@ while True:
             reply = HartFrame(FrameType.ACK,
                               request.command_number,
                               request.is_long_address,
-                              short_address,
+                              polling_address.get_value(),
                               long_address,
                               request.is_primary_master,
                               is_burst_mode,
