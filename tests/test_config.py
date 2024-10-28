a2bdc70a6ef7d7fc0a40dfe8120500b4f754fefe
gr0ker <24727134+gr0ker@users.noreply.github.com>
2024-10-28 11:03:25 +0100

    ✅ fix tests for config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import unittest
 
 from hartsim.config import Configuration
+from unittest.mock import patch, mock_open
 
 
 class TestConfig(unittest.TestCase):
@@ -8,8 +9,10 @@ class TestConfig(unittest.TestCase):
     def test_port_reading(self):
         expected = 'Qwerty123'
         data = { 'port': expected }
-        target = Configuration.from_dict(data)
-        self.assertEqual(target.port, expected)
+        with patch("builtins.open", mock_open(read_data="{}")):
+            with patch("json.load", return_value=data, ):
+                target = Configuration.load('some-path')
+                self.assertEqual(target.port, expected)
 
 if __name__ == '__main__':
     unittest.main()  # pragma: no cover
